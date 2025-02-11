42855a7814565d015efba0253fc30a7a84cc49d7
Oscar Liljefors <oscar.lf@live.se>
2025-02-11 19:38:42 +0100

    added JS for hamburger menu and fixed code

--- js-project-business-site-main/file.js
+++ js-project-business-site-main/file.js
@@ -4,4 +4,7 @@ document.getElementById("hidden-ham").onclick = function () { hamScript() };
 /* hamscript toggles between adding and removing the show class, which is used to hide and show the dropdown content */
 function hamScript() {
   document.getElementById("dropdown").classList.toggle("show");
+  document.getElementById("bar-one").classList.toggle("change");
+  document.getElementById("bar-two").classList.toggle("change");
+  document.getElementById("bar-three").classList.toggle("change");
 }
