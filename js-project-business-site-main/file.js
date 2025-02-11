f4a0e74b74d957dc178dbb88895a611f9abe80a1
Oscar Liljefors <oscar.lf@live.se>
2025-02-11 12:20:44 +0100

    added JS for hamburger and respective elements in html

---
+++ js-project-business-site-main/file.js
@@ -0,0 +1,7 @@
+// Get the div, and when the user clicks on it, execute myFunction
+document.getElementById("hidden-ham").onclick = function () { hamScript() };
+
+/* hamscript toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+function hamScript() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
